d521f6fd7c23758385a1daa1d0e4d522ca7c1f51
prakhar-kt <rahkarp@gmail.com>
2023-10-30 09:33:59 +0530

    changed the router to chi from pat and added a middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,18 +3,17 @@ package main
 import (
 	"net/http"
 
-	"github.com/bmizerany/pat"
-
+	"github.com/go-chi/chi/v5"
 	"github.com/prakhar-kt/go-webapp/pkg/config"
 	"github.com/prakhar-kt/go-webapp/pkg/handlers"
 )
 
 func routes(app *config.AppConfig) http.Handler {
 
-	mux := pat.New()
+	mux := chi.NewRouter()
 
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 
 	return mux
 
